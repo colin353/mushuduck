bb2312182a46a13837a60eff17e60f5b654b08c8
Ting Chen Leo Hsu <iteloo@gmail.com>
2014-01-04 13:44:00 -0500

    added timer

--- stage.py
+++ stage.py
@@ -1,4 +1,6 @@
 import game
+import threading
+import time
 
 class Stage:
 
@@ -19,11 +21,23 @@ class ProductionStage(Stage):
 		return 'Production'
 
 class TradingStage(Stage):
+	duration = 10
+
+	def __init__(self, game):
+		super(TradingStage, self).__init__(game)
+		threading.Timer(self.duration, timerEnd).start()
 
 	def begin(self):
 		# initialize prices if not already set
 		if not self.game.prices:
 			self.game.prices = {'tomato':10, 'blueberry':10, 'purple':10, 'corn':10}
-	
+
+		self.game.sendEventToAllPlayers('TimerBegin', {'duration':self.duration})
+
 	def type(self):
 		return 'Trading'
+
+	def timerEnd():
+		self.game.sendEventToAllPlayers('TimerEnd')
+		time.sleep(2)
+		self.game.nextStage()
