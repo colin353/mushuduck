1b27705615f0463f715af0e2217b31bb28007ef5
Ting Chen Leo Hsu <iteloo@gmail.com>
2014-01-04 23:27:21 -0500

    preliminary bidding
    mostly hardcoded

--- stage/bidding.py
+++ stage/bidding.py
@@ -4,9 +4,26 @@ class BiddingStage(stage.Stage):
 
 	def __init__(self, game):
 		super(ProductionStage, self).__init__(game)
+		self.topBids = {}
 
 	def type(self):
 		return 'Bidding'
 
 	def afterBegin(self):
+		# initialize a new bid
+		# todo: change to be dynamic and random
 		self.game.sendEventToAllPlayers('NewCard', {'index':0})
+
+	def bid(self, sender, data):
+		
+		bidAmount = data['bidAmount'] if 'bidAmount' in data else return "the action 'bid' failed to include a string named 'bidAmount'"
+		bidIndex = data['bidIndex'] if 'bidIndex' in data else return "the action 'bid' failed to include a string named 'bidIndex'"
+		self.currentBidIndex = bidIndex
+
+		if bidAmount is self.topBids[bidIndex]:
+			# if bid amount is the same as the top bid, return failure
+			return {'success':False}
+		else:
+			# otherwise, increase top bid, and return success
+			self.topBids[bidIndex] = bidAmount
+			return {'success':True}
