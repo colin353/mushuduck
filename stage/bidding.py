595d8ef4a99696f582600b3f1c82bc25f9f0af88
Colin MErkel <colin353@gmail.com>
2014-01-04 23:58:49 -0500

    what are the changes

--- stage/bidding.py
+++ stage/bidding.py
@@ -4,7 +4,8 @@ class BiddingStage(stage.Stage):
 
 	def __init__(self, game):
 		super(BiddingStage, self).__init__(game)
-		self.topBids = {}
+		# bids is a dictionary of stacks of bids indexed by the indices of the bids
+		self.bids = {}
 
 	def type(self):
 		return 'Bidding'
@@ -16,23 +17,39 @@ class BiddingStage(stage.Stage):
 
 	def bid(self, sender, data):
 		
+		# obtain bidAmount from data
 		if 'bidAmount' in data:
 			bidAmount = data['bidAmount']
 		else:
 			return "the action 'bid' failed to include a string named 'bidAmount'"
 		
+		# obtain bidIndex from data
 		if 'bidIndex' in data:
 			bidIndex = data['bidIndex']
 		else:
 			return "the action 'bid' failed to include a string named 'bidIndex'"
-		
-		# set current bid
-		self.currentBidIndex = bidIndex
 
-		if bidIndex in self.topBids and bidAmount is self.topBids[bidIndex]:
-			# if bid amount is the same as the top bid, return failure
+		# check if bid is successful
+		if bidIndex in self.bids and bidAmount is self.bids[bidIndex][-1]:
+			# if bid is the first, or has amount that is the same as the top bid, return failure
 			return {'success':False}
 		else:
-			# otherwise, increase top bid, and return success
-			self.topBids[bidIndex] = bidAmount
+			# otherwise, record top bid
+			bidder = self.game.playerWithHandler(sender)
+			newBid = Bid(bidder, bidIndex, bidAmount)
+			self.bids[bidIndex].append(newBid)
+
+			# announce new bid
+			for player in self.game.players:
+				winning = player is bidder
+				self.game.sendEventToPlayer(player, 'NewBid', {'winning':winning, 'winningBidAmount':bidAmount})
+			
+			# return success
 			return {'success':True}
+
+class Bid(object):
+
+	def __init__(self, bidder, index, amount):
+		self.bidder = player
+		self.index = index
+		self.amount = amount
