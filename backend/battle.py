683d245bb991365844db013ddee8291e800d5d51
Ting Chen Leo Hsu <iteloo@gmail.com>
2014-01-06 19:42:46 -0500

    deleted .pyc and added tomato prizes

--- backend/battle.py
+++ backend/battle.py
@@ -3,6 +3,7 @@ import helpers
 
 class BattleStage(stage.Stage):
 	duration = 5.0
+	battlePrizePerOpponent = 100.0
 
 	def __init__(self, game):
 		super(BattleStage, self).__init__(game)
@@ -22,11 +23,22 @@ class BattleStage(stage.Stage):
 		# data should be a dictionary of inventory counts
 		self.playerInventoryCounts[sender] = data
 
-		# if all players have updated their inventory counts, tell clients to display battle stats
+		# if all players have updated their inventory counts, display winning/losing messages on clients
 		if all(self.playerInventoryCounts.values()):
+			# determine winner
 			winner = max(self.playerInventoryCounts.items(), key=lambda p:p[1]['tomato'])[0]
+
+			# give winner money (must do before notifying winner)
+			prizeAmount = self.battlePrizePerOpponent*(len(self.game.players)-1)
+			self.game.sendEventToPlayer(winner, 'GoldGranted', {'amount':prize})
+
+			# notify each player that he or she either won or lost
 			for player in self.game.players:
-				self.game.sendEventToPlayer(player, 'DisplayMessage', {'title':"Battle!", 'text':"You %s the tomato war!" % ("won" if player is winner else "lost")})
+				if player is winner:
+					msg = "You won the tomato war!\nPrize: %d <span class='gold'></span>" % prizeAmount
+				else:
+					msg = "You lost the tomato war!"
+				self.game.sendEventToPlayer(player, 'DisplayMessage', {'title':"Battle!", 'text':msg})
 
 	def endStage(self):
 		self.game.nextStage()
